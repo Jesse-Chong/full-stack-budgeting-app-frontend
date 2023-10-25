c3efa30bcbd41c0ef8a5017ff7805292c063d997
Jesse Chong <jessechong@pursuit.org>
2023-10-25 12:44:24 -0400

    Added Navbar with bootstrap

--- budget-app/src/App.js
+++ budget-app/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Navbar
+          className="d-flex justify-content-between"
+          bg="dark"
+          variant="dark"
+          >
+            <Nav>
+              <Nav.Link href="/transactions">Transactions</Nav.Link>
+            </Nav>
+            <Navbar.Brand>Budget App</Navbar.Brand>
+            <Nav>
+              <Nav.Link href="/transactions/statistics">Statistics</Nav.Link>
+            </Nav>
+          </Navbar>
+      </div>
+    </Router>
   );
 }
 
